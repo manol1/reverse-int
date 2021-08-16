52c47ebbdc4b5a6a2f30d0d8cbd55b87f5a86611
manol1 <marsen1491@gmail.com>
2021-08-16 22:25:03 +0300

    feat: change index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let number = String(Math.abs(n));
+  let result = '';
+  let i = 0;
+  while (i < number.length) {
+    result = result + number[number.length - 1 - i];
+    i+=1;
+  }
+  return Number(result);
 }
